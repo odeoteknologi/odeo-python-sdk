58342baa3af790f9efcab69eb598c7bd2ebd531f
Rudi <rudi@odeo.co.id>
2022-02-02 21:51:03 +0700

    [TASK][OD-2956] Add bank account inquiry request tests
    Use assertIsNone instead of assertEqual to None for failed get banks test

--- tests/disbursement_service_test_case.py
+++ tests/disbursement_service_test_case.py
@@ -1,8 +1,12 @@
+import datetime
 import json
 import unittest
 
 import odeo.client
+from odeo.exceptions.insufficient_balance_error import InsufficientBalanceError
+from odeo.exceptions.invalid_bank_error import InvalidBankError
 from odeo.models.bank import Bank
+from odeo.models.bank_account import BankAccount
 from tests.service_test_case import ServiceTestCase
 
 
@@ -45,7 +49,81 @@ class DisbursementServiceTestCase(ServiceTestCase):
             })
         )
 
-        self.assertEqual(None, self.client.disbursement.get_banks())
+        self.assertIsNone(self.client.disbursement.get_banks())
+
+    def test_bank_account_inquiry(self):
+        self.adapter.register_uri(
+            'POST',
+            odeo.client.DEVELOPMENT_BASE_URL + '/dg/v1/bank-account-inquiry',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'xXZNi3mjrdmp6NM6drpXFyTcZGec4b4dxuA5dB/cHqI='
+            },
+            text=json.dumps({
+                'bank_id': 1,
+                'account_number': '1234567890',
+                'account_name': 'Agus Hartono',
+                'customer_name': 'Agus Hartono',
+                'fee': 1000,
+                'status': 50000,
+                'created_at': '1612137600',
+                'bank_account_inquiry_id': '123',
+                'validity': 100
+            })
+        )
+
+        self.assertEqual(
+            BankAccount(
+                1,
+                '1234567890',
+                'Agus Hartono',
+                'Agus Hartono',
+                1000,
+                50000,
+                datetime.datetime(2021, 2, 1),
+                '123',
+                100
+            ),
+            self.client.disbursement.bank_account_inquiry(
+                '1234567890', 1, 'Agus Hartono', True
+            )
+        )
+
+    def test_bank_account_inquiry_failed_invalid_bank(self):
+        self._create_failed_bank_account_inquiry_test(
+            InvalidBankError, 40002, 'Unknown error: INVALID BANK'
+        )
+
+    def test_bank_account_inquiry_failed_insufficient_balance(self):
+        self._create_failed_bank_account_inquiry_test(
+            InsufficientBalanceError, 40011, 'Unknown error: INSUFFICIENT BALANCE'
+        )
+
+    def _create_failed_bank_account_inquiry_test(self, error, error_code, message):
+        self.adapter.register_uri(
+            'POST',
+            odeo.client.DEVELOPMENT_BASE_URL + '/dg/v1/bank-account-inquiry',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'xXZNi3mjrdmp6NM6drpXFyTcZGec4b4dxuA5dB/cHqI='
+            },
+            status_code=400,
+            text=json.dumps({
+                'message': message,
+                'status_code': 400,
+                'error_code': error_code
+            })
+        )
+        with self.assertRaises(error) as ctx:
+            self.client.disbursement.bank_account_inquiry(
+                '1234567890', 1, 'Agus Hartono', True
+            )
+        self.assertEqual(str(ctx.exception), message)
+        self.assertEqual(ctx.exception.error_code, error_code)
 
 
 if __name__ == '__main__':
