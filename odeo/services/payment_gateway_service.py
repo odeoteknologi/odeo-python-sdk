0a49c2ced2e269e47d017019ba78a2f9b5ff2e7a
Rudi <rudi@odeo.co.id>
2022-02-04 17:07:46 +0700

    [TASK][OD-2956] Add get payment by id and reference_id request implementation

--- odeo/services/payment_gateway_service.py
+++ odeo/services/payment_gateway_service.py
@@ -1,7 +1,15 @@
-from odeo.services.base_service import BaseService
+from odeo.models.payment import Payment
+from odeo.services.base_service import BaseService, authenticated
 
 
 class PaymentGatewayService(BaseService):
 
-    def get_payment(self, by_payment_id: int, by_reference_id: str):
-        pass
+    @authenticated
+    def get_payment(self, by_payment_id: int = None, by_reference_id: str = None):
+        assert (by_payment_id is not None) ^ (by_reference_id is not None), \
+            'by_payment_id and by_reference_id parameters are mutually exclusive'
+
+        path = f"/pg/v1/payment/{by_payment_id}" if by_payment_id is not None else ''
+        path = f"/pg/v1/payment/reference-id/{by_reference_id}" if by_reference_id is not None else path
+
+        return Payment.from_json(self.request('GET', path).json())
