6303dedf225d05115e3c17f6c73dd20d190dd8e2
Rudi <rudi@odeo.co.id>
2022-02-04 19:13:26 +0700

    [TASK][OD-2956] Add list sub users request implementation

--- odeo/services/sub_user_service.py
+++ odeo/services/sub_user_service.py
@@ -1,10 +1,16 @@
-from odeo.services.base_service import BaseService
+from odeo.models.sub_user import SubUser
+from odeo.services.base_service import BaseService, authenticated
 
 
 class SubUserService(BaseService):
 
+    @authenticated
     def list_sub_users(self, page_token: str = None):
-        pass
+        params = {'page_token': page_token} if page_token is not None else {}
+        content = self.request('GET', '/sub-users', params).json()
+        sub_users = list(map(lambda sub_user: SubUser.from_json(sub_user), content['sub_users']))
+
+        return sub_users, content['next_page_token']
 
     def create_sub_user(self, email: str, name: str, phone_number: str):
         pass
