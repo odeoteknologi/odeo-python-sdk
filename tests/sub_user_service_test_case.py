10cba3f0b61004c1fdd68213fb7a0bb5d4e1f37e
Rudi <rudi@odeo.co.id>
2022-02-07 16:55:33 +0700

    [TASK][OD-2956] Add create & update sub user request tests

--- tests/sub_user_service_test_case.py
+++ tests/sub_user_service_test_case.py
@@ -2,6 +2,8 @@ import json
 import unittest
 
 import odeo.client
+from odeo.exceptions.general_error import GeneralError
+from odeo.exceptions.input_validation_error import InputValidationError
 from odeo.models.sub_user import SubUser
 from tests.service_test_case import ServiceTestCase
 
@@ -77,6 +79,186 @@ class SubUserServiceTestCase(ServiceTestCase):
             ([SubUser(123, 'Agus Hartono', '081234567890', 'agus@example.com')], 'abcdef'),
             self.client.sub_user.list_sub_users('abcdef')
         )
+
+    def test_create_sub_user(self):
+        self.adapter.register_uri(
+            'POST',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'IyMSYxV1An/jebiOlsVaWAz1XPlVQ663J5spsJlxlro='
+            },
+            text=json.dumps({
+                'user_id': '123',
+                'name': 'Agus Hartono',
+                'phone_number': '081234567890',
+                'email': 'agus@example.com'
+            })
+        )
+
+        self.assertEqual(
+            SubUser('123', 'Agus Hartono', '081234567890', 'agus@example.com'),
+            self.client.sub_user.create_sub_user(
+                'agus@example.com', 'Agus Hartono', '081234567890'
+            )
+        )
+
+    def test_create_sub_user_failed_email_invalid(self):
+        message = 'The email must be a valid email address'
+        self.adapter.register_uri(
+            'POST',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'DIQNg7zkrqYnM1bMFk563xv+Z2ljQOF8ptx5oR5LmZA='
+            },
+            status_code=400,
+            text=json.dumps({
+                'message': message,
+                'status_code': 400,
+                'error_code': 10001
+            })
+        )
+
+        with self.assertRaises(InputValidationError) as ctx:
+            self.client.sub_user.create_sub_user(
+                'agus-example.com', 'Agus Hartono', '081234567890'
+            )
+
+        self.assertEqual(str(ctx.exception), message)
+
+    def test_create_sub_user_failed_phone_number_already_registered(self):
+        message = 'Phone number registered'
+        self.adapter.register_uri(
+            'POST',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'IyMSYxV1An/jebiOlsVaWAz1XPlVQ663J5spsJlxlro='
+            },
+            status_code=400,
+            text=json.dumps({
+                'message': message,
+                'status_code': 400,
+                'error_code': 10000
+            })
+        )
+
+        with self.assertRaises(GeneralError) as ctx:
+            self.client.sub_user.create_sub_user(
+                'agus@example.com', 'Agus Hartono', '081234567890'
+            )
+
+        self.assertEqual(str(ctx.exception), message)
+
+    def test_update_sub_user(self):
+        self.adapter.register_uri(
+            'PUT',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users/123',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'tcw7lg3kRTbH4h/80Tv3cidIa+uCqZecHKRKgXtC+3Y='
+            },
+            text=json.dumps({
+                'user_id': '123',
+                'name': 'Agus Hartono',
+                'phone_number': '081234567890',
+                'email': 'agus@example.com'
+            })
+        )
+
+        self.assertEqual(
+            SubUser('123', 'Agus Hartono', '081234567890', 'agus@example.com'),
+            self.client.sub_user.update_sub_user(
+                123, 'agus@example.com', 'Agus Hartono', '081234567890'
+            )
+        )
+
+    def test_update_sub_user_failed_email_invalid(self):
+        message = 'The email must be a valid email address'
+        self.adapter.register_uri(
+            'PUT',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users/123',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'qcZEJpP9vS1KNRVqCqS5qzyxmEUxn8xi8pBgEprySaU='
+            },
+            status_code=400,
+            text=json.dumps({
+                'message': message,
+                'status_code': 400,
+                'error_code': 10001
+            })
+        )
+
+        with self.assertRaises(InputValidationError) as ctx:
+            self.client.sub_user.update_sub_user(
+                123, 'agus-example.com', 'Agus Hartono', '081234567890'
+            )
+
+        self.assertEqual(str(ctx.exception), message)
+
+    def test_update_sub_user_failed_user_not_found(self):
+        message = 'Data not found'
+        self.adapter.register_uri(
+            'PUT',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users/123',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'tcw7lg3kRTbH4h/80Tv3cidIa+uCqZecHKRKgXtC+3Y='
+            },
+            status_code=400,
+            text=json.dumps({
+                'message': message,
+                'status_code': 400,
+                'error_code': 10000
+            })
+        )
+
+        with self.assertRaises(GeneralError) as ctx:
+            self.client.sub_user.update_sub_user(
+                123, 'agus@example.com', 'Agus Hartono', '081234567890'
+            )
+
+        self.assertEqual(str(ctx.exception), message)
+
+    def test_update_sub_user_failed_phone_number_already_registered(self):
+        message = 'Phone number registered'
+        self.adapter.register_uri(
+            'PUT',
+            odeo.client.DEVELOPMENT_BASE_URL + '/sub-users/123',
+            request_headers={
+                'Authorization': 'Bearer ' + self.access_token,
+                'Content-Type': 'application/json',
+                'X-Odeo-Timestamp': '1612137600',
+                'X-Odeo-Signature': 'tcw7lg3kRTbH4h/80Tv3cidIa+uCqZecHKRKgXtC+3Y='
+            },
+            status_code=400,
+            text=json.dumps({
+                'message': message,
+                'status_code': 400,
+                'error_code': 10000
+            })
+        )
+
+        with self.assertRaises(GeneralError) as ctx:
+            self.client.sub_user.update_sub_user(
+                123, 'agus@example.com', 'Agus Hartono', '081234567890'
+            )
+
+        self.assertEqual(str(ctx.exception), message)
 
 
 if __name__ == '__main__':
