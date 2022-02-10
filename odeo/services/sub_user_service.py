1cf6cc7bd0f66f4c28cd36e5fe3ac35094d0e0df
Rudi <rudi@odeo.co.id>
2022-02-10 20:29:39 +0700

    [TASK][OD-2956] Update list sub users request to return ListSubUserResponse

--- odeo/services/sub_user_service.py
+++ odeo/services/sub_user_service.py
@@ -1,3 +1,4 @@
+from odeo.models.list_sub_user_response import ListSubUserResponse
 from odeo.models.sub_user import SubUser
 from odeo.services.base_service import BaseService, authenticated
 
@@ -7,10 +8,9 @@ class SubUserService(BaseService):
     @authenticated
     def list_sub_users(self, page_token: str = None):
         params = {'page_token': page_token} if page_token is not None else {}
-        content = self.request('GET', '/sub-users', params).json()
-        sub_users = list(map(lambda sub_user: SubUser.from_json(sub_user), content['sub_users']))
+        response = self.request('GET', '/sub-users', params)
 
-        return sub_users, content['next_page_token']
+        return ListSubUserResponse.from_json(response.json())
 
     @authenticated
     def create_sub_user(self, email: str, name: str, phone_number: str):
