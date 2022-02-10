ceed6e68597ebcf25bef5a75e3d9746fb092d086
Rudi <rudi@odeo.co.id>
2022-02-10 15:43:31 +0700

    [TASK][OD-2956] Implement list transfers request

--- odeo/services/cash_service.py
+++ odeo/services/cash_service.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from odeo.models.list_transfers_response import ListTransfersResponse
 from odeo.models.request import Request
 from odeo.models.transfer import Transfer
 from odeo.services.base_service import BaseService, authenticated
@@ -15,14 +18,29 @@ class CashService(BaseService):
             lambda c: list(map(lambda transfer: Transfer.from_json(transfer), c['transfers']))
         )
 
+    @authenticated
     def list_transfers(
             self,
             reference_ids: list[str] = None,
-            start_date: str = None,
-            end_date: str = None,
+            start_date: datetime = None,
+            end_date: datetime = None,
             page_token: str = None
     ):
-        pass
+        params = {}
+
+        if reference_ids is not None:
+            for i in range(0, len(reference_ids)):
+                params[f'reference_ids[{i}]'] = reference_ids[i]
+        if start_date is not None:
+            params['start_date'] = int(start_date.timestamp())
+        if end_date is not None:
+            params['end_date'] = int(end_date.timestamp())
+        if page_token is not None:
+            params['page_token'] = page_token
+
+        response = self.request('GET', '/cash/transfers', params)
+
+        return ListTransfersResponse.from_json(response.json())
 
     def create_va_topup(self, amount: int, user_id: int = None):
         pass
